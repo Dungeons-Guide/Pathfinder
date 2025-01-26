09904c9dd17d5bf179c145a94d84d5ad3d790446
syeyoung <cyoung06@naver.com>
2025-01-26 22:29:30 +0900

    - fix buildscript

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # login
 aws ecr get-login-password --region us-east-1 --profile dg-profile | docker login --username AWS --password-stdin 471112686938.dkr.ecr.us-east-1.amazonaws.com
 
-docker buildx build --platform linux/arm64,linux/amd64 -t 471112686938.dkr.ecr.us-east-1.amazonaws.com/processor:1.1 --push .
+docker buildx build --platform linux/arm64,linux/amd64 -t 471112686938.dkr.ecr.us-east-1.amazonaws.com/processor:2.0 --push .
 
 
